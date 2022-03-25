25f71d55f8f5f6e685e1f2606dd3d4a2194fb294
awais512 <raza.awais25@gmail.com>
2022-03-25 09:00:03 +0500

    Cart screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,83 +1,86 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <div>
       {/* Top Header */}
-      <div className="Announcement ">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 d-flex align-items-center display-none">
+      <div className='Announcement '>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 d-flex align-items-center display-none'>
               <p>+255 768 356 890</p>
               <p>info@zpunet.com</p>
             </div>
-            <div className=" col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center">
-              <Link to="">
-                <i className="fab fa-facebook-f"></i>
+            <div className=' col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center'>
+              <Link to=''>
+                <i className='fab fa-facebook-f'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-instagram"></i>
+              <Link to=''>
+                <i className='fab fa-instagram'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-linkedin-in"></i>
+              <Link to=''>
+                <i className='fab fa-linkedin-in'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-youtube"></i>
+              <Link to=''>
+                <i className='fab fa-youtube'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-pinterest-p"></i>
+              <Link to=''>
+                <i className='fab fa-pinterest-p'></i>
               </Link>
             </div>
           </div>
         </div>
       </div>
       {/* Header */}
-      <div className="header">
-        <div className="container">
+      <div className='header'>
+        <div className='container'>
           {/* MOBILE HEADER */}
-          <div className="mobile-header">
-            <div className="container ">
-              <div className="row ">
-                <div className="col-6 d-flex align-items-center">
-                  <Link className="navbar-brand" to="/">
-                    <img alt="logo" src="/images/logo.png" />
+          <div className='mobile-header'>
+            <div className='container '>
+              <div className='row '>
+                <div className='col-6 d-flex align-items-center'>
+                  <Link className='navbar-brand' to='/'>
+                    <img alt='logo' src='/images/logo.png' />
                   </Link>
                 </div>
-                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
-                  <div className="btn-group">
+                <div className='col-6 d-flex align-items-center justify-content-end Login-Register'>
+                  <div className='btn-group'>
                     <button
-                      type="button"
-                      className="name-button dropdown-toggle"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
+                      type='button'
+                      className='name-button dropdown-toggle'
+                      data-toggle='dropdown'
+                      aria-haspopup='true'
+                      aria-expanded='false'
                     >
-                      <i class="fas fa-user"></i>
+                      <i class='fas fa-user'></i>
                     </button>
-                    <div className="dropdown-menu">
-                      <Link className="dropdown-item" to="/profile">
+                    <div className='dropdown-menu'>
+                      <Link className='dropdown-item' to='/profile'>
                         Profile
                       </Link>
 
-                      <Link className="dropdown-item" to="#">
+                      <Link className='dropdown-item' to='#'>
                         Logout
                       </Link>
                     </div>
                   </div>
-                  <Link to="/cart" className="cart-mobile-icon">
-                    <i className="fas fa-shopping-bag"></i>
-                    <span className="badge">4</span>
+                  <Link to='/cart' className='cart-mobile-icon'>
+                    <i className='fas fa-shopping-bag'></i>
+                    <span className='badge'>{cartItems.length}</span>
                   </Link>
                 </div>
-                <div className="col-12 d-flex align-items-center">
-                  <form className="input-group">
+                <div className='col-12 d-flex align-items-center'>
+                  <form className='input-group'>
                     <input
-                      type="search"
-                      className="form-control rounded search"
-                      placeholder="Search"
+                      type='search'
+                      className='form-control rounded search'
+                      placeholder='Search'
                     />
-                    <button type="submit" className="search-button">
+                    <button type='submit' className='search-button'>
                       search
                     </button>
                   </form>
@@ -87,50 +90,50 @@ const Header = () => {
           </div>
 
           {/* PC HEADER */}
-          <div className="pc-header">
-            <div className="row">
-              <div className="col-md-3 col-4 d-flex align-items-center">
-                <Link className="navbar-brand" to="/">
-                  <img alt="logo" src="/images/logo.png" />
+          <div className='pc-header'>
+            <div className='row'>
+              <div className='col-md-3 col-4 d-flex align-items-center'>
+                <Link className='navbar-brand' to='/'>
+                  <img alt='logo' src='/images/logo.png' />
                 </Link>
               </div>
-              <div className="col-md-6 col-8 d-flex align-items-center">
-                <form className="input-group">
+              <div className='col-md-6 col-8 d-flex align-items-center'>
+                <form className='input-group'>
                   <input
-                    type="search"
-                    className="form-control rounded search"
-                    placeholder="Search"
+                    type='search'
+                    className='form-control rounded search'
+                    placeholder='Search'
                   />
-                  <button type="submit" className="search-button">
+                  <button type='submit' className='search-button'>
                     search
                   </button>
                 </form>
               </div>
-              <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
-                <div className="btn-group">
+              <div className='col-md-3 d-flex align-items-center justify-content-end Login-Register'>
+                <div className='btn-group'>
                   <button
-                    type="button"
-                    className="name-button dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
+                    type='button'
+                    className='name-button dropdown-toggle'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
                   >
                     Hi, Admin Doe
                   </button>
-                  <div className="dropdown-menu">
-                    <Link className="dropdown-item" to="/profile">
+                  <div className='dropdown-menu'>
+                    <Link className='dropdown-item' to='/profile'>
                       Profile
                     </Link>
 
-                    <Link className="dropdown-item" to="#">
+                    <Link className='dropdown-item' to='#'>
                       Logout
                     </Link>
                   </div>
                 </div>
 
-                <Link to="/cart">
-                  <i className="fas fa-shopping-bag"></i>
-                  <span className="badge">4</span>
+                <Link to='/cart'>
+                  <i className='fas fa-shopping-bag'></i>
+                  <span className='badge'>{cartItems.length}</span>
                 </Link>
               </div>
             </div>
