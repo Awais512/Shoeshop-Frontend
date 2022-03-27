710777e894dde1a1d9c6828fd5749fa1089b63c7
awais512 <raza.awais25@gmail.com>
2022-03-27 10:11:39 +0500

    Payment method screen

--- src/Redux/Actions/CartActions.js
+++ src/Redux/Actions/CartActions.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../Constants/CartConstants';
 
+//Add to cart
 export const addTocart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
 
@@ -23,6 +25,7 @@ export const addTocart = (id, qty) => async (dispatch, getState) => {
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
 
+//Remove from Cart
 export const removeFromCart = (id) => async (dispatch, getState) => {
   dispatch({
     type: CART_REMOVE_ITEM,
@@ -39,4 +42,14 @@ export const saveShippingAddress = (data) => async (dispatch) => {
   });
 
   localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+//Save Payment Method
+export const savePaymentMethod = (data) => async (dispatch) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
+
+  localStorage.setItem('paymentMethod', JSON.stringify(data));
 };
