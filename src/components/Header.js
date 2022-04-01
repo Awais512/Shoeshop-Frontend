604af1bc3ec1065525521ff19e28078798bb4fe7
awais512 <raza.awais25@gmail.com>
2022-04-01 09:57:52 +0500

    Minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../Redux/Actions/userActions";
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../Redux/Actions/userActions';
 
 const Header = () => {
   const [keyword, setKeyword] = useState();
@@ -22,71 +22,71 @@ const Header = () => {
     if (keyword.trim()) {
       history.push(`/search/${keyword}`);
     } else {
-      history.push("/");
+      history.push('/');
     }
   };
   return (
     <div>
       {/* Top Header */}
-      <div className="Announcement ">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 d-flex align-items-center display-none">
-              <p>+255 768 356 890</p>
-              <p>info@zpunet.com</p>
+      <div className='Announcement '>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 d-flex align-items-center display-none'>
+              <p>+92 311 635 4049</p>
+              <p>awaisc509@gmail.com</p>
             </div>
-            <div className=" col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center">
-              <Link to="">
-                <i className="fab fa-facebook-f"></i>
+            <div className=' col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center'>
+              <Link to=''>
+                <i className='fab fa-facebook-f'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-instagram"></i>
+              <Link to=''>
+                <i className='fab fa-instagram'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-linkedin-in"></i>
+              <Link to=''>
+                <i className='fab fa-linkedin-in'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-youtube"></i>
+              <Link to=''>
+                <i className='fab fa-youtube'></i>
               </Link>
-              <Link to="">
-                <i className="fab fa-pinterest-p"></i>
+              <Link to=''>
+                <i className='fab fa-pinterest-p'></i>
               </Link>
             </div>
           </div>
         </div>
       </div>
       {/* Header */}
-      <div className="header">
-        <div className="container">
+      <div className='header'>
+        <div className='container'>
           {/* MOBILE HEADER */}
-          <div className="mobile-header">
-            <div className="container ">
-              <div className="row ">
-                <div className="col-6 d-flex align-items-center">
-                  <Link className="navbar-brand" to="/">
-                    <img alt="logo" src="/images/logo.png" />
+          <div className='mobile-header'>
+            <div className='container '>
+              <div className='row '>
+                <div className='col-6 d-flex align-items-center'>
+                  <Link className='navbar-brand' to='/'>
+                    <img alt='logo' src='/images/logo.png' />
                   </Link>
                 </div>
-                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
+                <div className='col-6 d-flex align-items-center justify-content-end Login-Register'>
                   {userInfo ? (
-                    <div className="btn-group">
+                    <div className='btn-group'>
                       <button
-                        type="button"
-                        className="name-button dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
+                        type='button'
+                        className='name-button dropdown-toggle'
+                        data-toggle='dropdown'
+                        aria-haspopup='true'
+                        aria-expanded='false'
                       >
-                        <i class="fas fa-user"></i>
+                        <i class='fas fa-user'></i>
                       </button>
-                      <div className="dropdown-menu">
-                        <Link className="dropdown-item" to="/profile">
+                      <div className='dropdown-menu'>
+                        <Link className='dropdown-item' to='/profile'>
                           Profile
                         </Link>
 
                         <Link
-                          className="dropdown-item"
-                          to="#"
+                          className='dropdown-item'
+                          to='#'
                           onClick={logoutHandler}
                         >
                           Logout
@@ -94,42 +94,42 @@ const Header = () => {
                       </div>
                     </div>
                   ) : (
-                    <div className="btn-group">
+                    <div className='btn-group'>
                       <button
-                        type="button"
-                        className="name-button dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
+                        type='button'
+                        className='name-button dropdown-toggle'
+                        data-toggle='dropdown'
+                        aria-haspopup='true'
+                        aria-expanded='false'
                       >
-                        <i class="fas fa-user"></i>
+                        <i class='fas fa-user'></i>
                       </button>
-                      <div className="dropdown-menu">
-                        <Link className="dropdown-item" to="/login">
+                      <div className='dropdown-menu'>
+                        <Link className='dropdown-item' to='/login'>
                           Login
                         </Link>
 
-                        <Link className="dropdown-item" to="/register">
+                        <Link className='dropdown-item' to='/register'>
                           Register
                         </Link>
                       </div>
                     </div>
                   )}
 
-                  <Link to="/cart" className="cart-mobile-icon">
-                    <i className="fas fa-shopping-bag"></i>
-                    <span className="badge">{cartItems.length}</span>
+                  <Link to='/cart' className='cart-mobile-icon'>
+                    <i className='fas fa-shopping-bag'></i>
+                    <span className='badge'>{cartItems.length}</span>
                   </Link>
                 </div>
-                <div className="col-12 d-flex align-items-center">
-                  <form onSubmit={submitHandler} className="input-group">
+                <div className='col-12 d-flex align-items-center'>
+                  <form onSubmit={submitHandler} className='input-group'>
                     <input
-                      type="search"
-                      className="form-control rounded search"
-                      placeholder="Search"
+                      type='search'
+                      className='form-control rounded search'
+                      placeholder='Search'
                       onChange={(e) => setKeyword(e.target.value)}
                     />
-                    <button type="submit" className="search-button">
+                    <button type='submit' className='search-button'>
                       search
                     </button>
                   </form>
@@ -139,46 +139,46 @@ const Header = () => {
           </div>
 
           {/* PC HEADER */}
-          <div className="pc-header">
-            <div className="row">
-              <div className="col-md-3 col-4 d-flex align-items-center">
-                <Link className="navbar-brand" to="/">
-                  <img alt="logo" src="/images/logo.png" />
+          <div className='pc-header'>
+            <div className='row'>
+              <div className='col-md-3 col-4 d-flex align-items-center'>
+                <Link className='navbar-brand' to='/'>
+                  <img alt='logo' src='/images/logo.png' />
                 </Link>
               </div>
-              <div className="col-md-6 col-8 d-flex align-items-center">
-                <form onSubmit={submitHandler} className="input-group">
+              <div className='col-md-6 col-8 d-flex align-items-center'>
+                <form onSubmit={submitHandler} className='input-group'>
                   <input
-                    type="search"
-                    className="form-control rounded search"
-                    placeholder="Search"
+                    type='search'
+                    className='form-control rounded search'
+                    placeholder='Search'
                     onChange={(e) => setKeyword(e.target.value)}
                   />
-                  <button type="submit" className="search-button">
+                  <button type='submit' className='search-button'>
                     search
                   </button>
                 </form>
               </div>
-              <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
+              <div className='col-md-3 d-flex align-items-center justify-content-end Login-Register'>
                 {userInfo ? (
-                  <div className="btn-group">
+                  <div className='btn-group'>
                     <button
-                      type="button"
-                      className="name-button dropdown-toggle"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
+                      type='button'
+                      className='name-button dropdown-toggle'
+                      data-toggle='dropdown'
+                      aria-haspopup='true'
+                      aria-expanded='false'
                     >
                       Hi, {userInfo.name}
                     </button>
-                    <div className="dropdown-menu">
-                      <Link className="dropdown-item" to="/profile">
+                    <div className='dropdown-menu'>
+                      <Link className='dropdown-item' to='/profile'>
                         Profile
                       </Link>
 
                       <Link
-                        className="dropdown-item"
-                        to="#"
+                        className='dropdown-item'
+                        to='#'
                         onClick={logoutHandler}
                       >
                         Logout
@@ -187,14 +187,14 @@ const Header = () => {
                   </div>
                 ) : (
                   <>
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
+                    <Link to='/register'>Register</Link>
+                    <Link to='/login'>Login</Link>
                   </>
                 )}
 
-                <Link to="/cart">
-                  <i className="fas fa-shopping-bag"></i>
-                  <span className="badge">{cartItems.length}</span>
+                <Link to='/cart'>
+                  <i className='fas fa-shopping-bag'></i>
+                  <span className='badge'>{cartItems.length}</span>
                 </Link>
               </div>
             </div>
